80ab6ccbc42dcd458fcc05b9721e09598d4a3e48
Mart Velema <mart.velema@student.nhlstenden.com>
2025-04-04 15:02:48 +0200

    Fixed Style not having getters/setters, Fixed colours not rendering

--- src/com/nhlstenden/style/Style.java
+++ src/com/nhlstenden/style/Style.java
@@ -4,14 +4,44 @@ import java.awt.*;
 
 public class Style
 {
-    public Font font;
-    public Color color;
-    public int indentation;
+    private Font font;
+    private Color color;
+    private int indentation;
 
     public Style(Font font, Color color, int indentation)
     {
         this.font = font;
         this.color = color;
+        this.indentation = indentation;
+    }
+
+    public Font getFont()
+    {
+        return font;
+    }
+
+    public void setFont(Font font)
+    {
+        this.font = font;
+    }
+
+    public Color getColor()
+    {
+        return color;
+    }
+
+    public void setColor(Color color)
+    {
+        this.color = color;
+    }
+
+    public int getIndentation()
+    {
+        return indentation;
+    }
+
+    public void setIndentation(int indentation)
+    {
         this.indentation = indentation;
     }
 }
